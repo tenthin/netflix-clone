5617691ff3ea011dd938bab0f6ac043919a00410
Tenzin <tenthinten@gmail.com>
2022-03-07 13:09:28 +0530

    pull information for the row

--- src/Row.js
+++ src/Row.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 
 function Row({ title }) {
     const [movies,setMovies] = useState([]);
     
+    // A snippet of code which runs based on a specific condition/variable
+    useEffect(() => {
+        // 
+    },[])
 
-  return (
-    <div>
-        <h1>{title}</h1>
+    return (
+        <div>
+            <h1>{title}</h1>
 
-        {/* container --> posters */}
-    </div>
-  )
+            {/* container --> posters */}
+        </div>
+    )
 }
 
 export default Row
