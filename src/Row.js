21e99d57dc93dd7f6ed27dd8a5bffebd9f5b7f47
Tenzin <tenthinten@gmail.com>
2022-03-11 18:19:47 +0530

    console log the request data

--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,16 @@
 import React, { useState,useEffect } from 'react'
 import axios from './axios';
 
-function Row({ title }) {
+function Row({ title,fetchUrl }) {
     const [movies,setMovies] = useState([]);
     
     // A snippet of code which runs based on a specific condition/variable
     useEffect(() => {
         // 
         async function fetchData() {
-
+            const request = await axios.get(fetchUrl);
+            console.log(request);
+            return request;
         }
         fetchData();
     },[])
