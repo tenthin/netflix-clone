79a0d6af34f91b00149060ec58228b76d41a3bfc
Tenzin <tenthinten@gmail.com>
2022-03-16 18:26:41 +0530

    css for rows

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,9 @@
 import React, { useState,useEffect } from 'react'
 import axios from './axios';
+import "./Row.css"
+
+const base_url = "https://image.tmdb.org/t/p/original"
+
 
 function Row({ title,fetchUrl }) {
     const [movies,setMovies] = useState([]);
@@ -22,7 +26,11 @@ function Row({ title,fetchUrl }) {
             <div className="row__posters">
             {/* several row poster */}
             {movies.map(movie => (
-                <img src={movie.poster_path} alt={movie.name} />
+                <img
+                    className="row__poster" 
+                    src={`${base_url}${movie.poster_path}`} 
+                    alt={movie.name} 
+                />
             ))}
             </div>
         </div>
